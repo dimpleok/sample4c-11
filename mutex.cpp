8773e91a2a5ed548c50414cb64b2a760be33f3e4
dimpleok <wanjiushi@126.com>
2025-05-10 19:10:25 +0800

    Format code.

--- mutex.cpp
+++ mutex.cpp
@@ -4,23 +4,22 @@
 #include <mutex>
 
 std::map<std::string, bool> pool;
-
 std::mutex m;
-void pushUrl(const std::string & url, bool b)
+
+void pushUrl(const std::string& url, bool b)
 {
-   std::lock_guard<std::mutex>  lock(m);
+   std::lock_guard<std::mutex> lock(m);
    pool[url] = b;
 }
 
 int main()
 {
-    std::thread t1( pushUrl, "www.sina.com.cn", true);
-    std::thread t2( pushUrl, "www.google.com", false);
+    std::thread t1(pushUrl, "www.sina.com.cn", true);
+    std::thread t2(pushUrl, "www.google.com", false);
     t1.join();
     t2.join();
-    for(auto e : pool) {
+    for (auto e : pool) {
        std::cout << e.first  << " " << e.second << std::endl;
     }
     return 0;
 }
-
