612b88237f430af652beb5e487423dd946b762ed
dimpleok <wanjiushi@126.com>
2025-05-10 19:07:34 +0800

    Format code.

--- array.cpp
+++ array.cpp
@@ -9,8 +9,8 @@ int main()
    arr.fill(99);
 
    arr[0] = 1;
-   std::copy( std::begin(arr), std::end(arr),
-              std::ostream_iterator<int>(std::cout, " ")
+   std::copy(std::begin(arr), std::end(arr),
+             std::ostream_iterator<int>(std::cout, " ")
             );
    std::cout << "\n" << "size=" << arr.size() << std::endl;
    std::cout << arr.front() << std::endl;
@@ -18,7 +18,7 @@ int main()
 
    std::array<int, 3> arr2{1, 2, 3};
    arr2[1] *= 10;
-   for(auto i : arr2) {
+   for (auto i : arr2) {
        std::cout << i << " ";
    }
    return 0;
