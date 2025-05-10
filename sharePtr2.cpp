077787d75d4cbe5244d30b1269b412f0c4a3d805
dimpleok <wanjiushi@126.com>
2025-05-10 19:18:37 +0800

    Use explicit pointer to compare.

--- sharePtr2.cpp
+++ sharePtr2.cpp
@@ -20,7 +20,7 @@ std::shared_ptr<Node<T>> makeLink(const std::vector<T>& v)
     std::shared_ptr<Node<T>> head, lastNode; 
     for(auto e : v) {
         std::shared_ptr<Node<T>> pNode = std::make_shared<Node<T>>(e, nullptr);
-        if(lastNode) {
+        if(lastNode != nullptr) {
             lastNode->next =  pNode;
             lastNode = pNode;
         } else {
